86ff6705c75ee80ad9e704034f4789e0d7faceae
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-05 00:08:52 -0400

    mvp commit for variable.js

---
+++ script.js
@@ -0,0 +1,88 @@
+// In the following code, "Mark" is a string. What is name (answer with 1 word)?
+
+var name = "Mark";
+
+variable
+
+// In one word, if it is enclosed in quotation marks, what is it?
+
+string
+
+// This statement...
+userName = "buddy5000";
+// ...is correct only if the variable has already been _______.
+
+declared
+
+// form a statement that declares the variable punchline and assigns a value to it
+
+var punchline = 1;
+
+// In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
+
+var nickname = "Satchmo";
+
+// Declare the variable teamName.
+
+var teamName;
+
+// In a single statement declare a variable and assign a string of your choice to it. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var name = "Justin";
+
+// Declare a variable. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var teamName;
+
+// Assign a string of your choice to a variable of your choice that has already been declared. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var teamName = "Detroit Pistons";
+
+// This statement has already been coded.
+var bestMan = "Charlie";
+// Assign the variable a new string.
+
+var bestMan = "Emanuel";
+
+// Declare a variable without defining it. Then, in a second statement, assign it a string. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var name ;
+name = "Justin";
+
+// Time goal: 30 seconds. (The timer starts when you click in the answer field.) In a single statement declare a variable of your choice and assign a string of your choice to it. You've learned only one rule for naming variables so far, so it's the only one I'll hold you to. (The timer starts when you click in the answer field.)
+
+var city = "Detroit";
+
+// Time goal: 15 seconds. Declare a variable of your choice. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var total;
+
+// Time goal: 20 seconds. Assign a string of your choice to a variable of your choice that has already been declared. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var total = 10;
+
+// Time goal: 20 seconds. This statement has already been coded.
+var bestMan = "Charlie";
+// Assign the variable a new string of your choice.
+
+bestMan = "Chachovar";
+
+// Time goal: 35 seconds. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice. (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+
+var location;
+location = "Michigan";
+
+// Time goal: 40 seconds. In the first statement declare a variable of your choice and assign it a string of your choice. In the second statement change the value of the variable to a different string of your choice
+
+var statement;
+statement = "Hello World";
+
+// Live coding exercise: Declare a variable of your choice and assign it a string of your choice. Then code an alert, specifying the variable, not the string, as the message. Click the Run It button to run your code live. Type out the code, don't copy and paste it. If you copy and paste it from the screen showing the correct code, it may not work.
+
+var greeting = "Good Afternoon";
+alert(greeting);
+
+// Live coding exercise: Declare a variable of your choice and assign it a string that's exactly the same as the variable name, but enclosed in quotation marks so JavaScript recognizes it as a string. Then code an alert, specifying the variable, not the string, as the message. Click the Run It button to run your code live. Type out the code, don't copy and paste it. If you copy and paste it from the screen showing the correct code, it may not work.
+
+var name = "name";
+alert(name);
